333e0679681150a1154441df7797ac87a7a27846
hjmorrow23 <hjmorrow23@gmail.com>
2024-07-06 14:59:48 -0400

    chore: gather packages and start theme setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App).mount('#app')
+
+app.use(PrimeVue, {
+    unstyled: true
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  test: {}
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv('', process.cwd())
+  return {
+    plugins: [vue()],
+    test: {},
+    define: {
+      __APP_ENV__: env.APP_ENV
+    }
+  }
 })
